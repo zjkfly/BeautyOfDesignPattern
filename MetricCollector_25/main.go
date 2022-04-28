d0fe7c2ac526bc2e8c04c465630f86185e16e996
zjkfly <1562597324@qq.com>
2022-04-29 00:43:30 +0800

    chore 接口统计框架v1.0

--- MetricCollector_25/main.go
+++ MetricCollector_25/main.go
@@ -1,7 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"time"
+)
+
+type Metrics struct {
+	responseTimes map[string][]int64
+	timestamps    map[string][]int64
+}
+type MetricsInterface interface {
+	recordResponseTime(apiName string, responseTs int64) error
+	recordTimestamp(apiName string, responseTs int64) error
+	startRepeatedReport(apiName string, period int64) error
+	max(apiName string, startTs, endTs int64) (int64, error)
+	min(apiName string, startTs, endTs int64) (int64, error)
+}
+
+func (m *Metrics) recordResponseTime(apiName string, responseTs int64) error {
+	m.timestamps[apiName] = append(m.timestamps[apiName], responseTs)
+	return nil
+}
+func (m *Metrics) recordTimestamp(apiName string, timestamp int64) error {
+	m.timestamps[apiName] = append(m.timestamps[apiName], timestamp)
+	return nil
+}
+
+func (m *Metrics) startRepeatedReport(apiName string, period int64) error {
+	panic("implement me")
+}
+
+func (m *Metrics) max(apiName string, startTs, endTs int64) (int64, error) {
+	panic("implement me")
+}
+
+func (m *Metrics) min(apiName string, startTs, endTs int64) (int64, error) {
+	panic("implement me")
+}
+
+type User struct {
+	account  string
+	password string
+	sex int
+	addr string
+}
+
+func NewUser(account string, password string, sex int, addr string) *User {
+	return &User{account: account, password: password, sex: sex, addr: addr}
+}
+
+type UserInterface interface {
+	register(user User) error
+	login(account, password string) *User
+}
+type UserController struct {
+	metricsInterface MetricsInterface
+}
+
+func NewUserController() UserInterface {
+	return &UserController{
+		metricsInterface: &Metrics{},
+	}
+}
+
+func (u *UserController) register(user User) error {
+	apiName := "register"
+	u.metricsInterface.recordTimestamp(apiName, time.Now().Unix())
+	/*
+		todo 业务操作
+	*/
+	u.metricsInterface.recordResponseTime(apiName, time.Now().Unix())
+
+	return nil
+}
+func (u *UserController) login(account, password string) *User {
+	apiName := "login"
+	u.metricsInterface.recordTimestamp(apiName, time.Now().Unix())
+	/*
+		todo 业务操作
+	*/
+	u.metricsInterface.recordResponseTime(apiName, time.Now().Unix())
+	return nil
+}
 
 func main() {
-	fmt.Println("BeautyOfDesignPattern is running")
+	userController := NewUserController()
+	userController.register(*NewUser("zjk","123456",1/*1 man 2 woman 3 unknown*/,"白京"))
+	userController.login("zjk", "123456")
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BeautyOfDesignPattern
- da
+## 背景
+最近在学王争的《设计模式之美》，由于原文是java编写，为了更好理解并且有实操，所以<br>
+有这个项目记录golang的代码实现文中的工程。
+## 参考
+王争-《设计模式之美》;https://time.geekbang.org/column/intro/100039001
+
+## 实现细节
+MetricsCollector_25目录对应课程25相关代码
